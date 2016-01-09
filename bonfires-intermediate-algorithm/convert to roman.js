0b33efdf535f698de77ad9400a67ebce4a590cb7
Rahul <Rahulreddy160@gmail.com>
2016-01-09 22:33:37 +0100

    Update convert to roman.js

--- bonfires-intermediate-algorithm/convert to roman.js
+++ bonfires-intermediate-algorithm/convert to roman.js
@@ -2,8 +2,8 @@
 var convert= function(num){
     var numbers = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
     var roman = ['I','IV','V','IX','X','XL','L','XC','C','CD','D','CM','M'];
-    numbers.reverse();
-    roman.reverse();
+    numbers.reverse();//not necessary if array in decreasing order.
+    roman.reverse();//not necessary if array in decreasing order.
     var result = '';
   for(var i=0 ; i < numbers.length;i++){
     while(numbers[i]<= num){
