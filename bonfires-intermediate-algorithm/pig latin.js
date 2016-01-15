0a5b64071c6d88ac68f701f2caa3be4522f95fe2
Rahul <Rahulreddy160@gmail.com>
2016-01-15 18:28:16 +0100

    Update pig latin.js

--- bonfires-intermediate-algorithm/pig latin.js
+++ bonfires-intermediate-algorithm/pig latin.js
@@ -1,3 +1,4 @@
+//solution 1
 function translate(str) {
   // Create variables to be used
   var newstr = '';
@@ -21,3 +22,24 @@ function translate(str) {
 }
 
 translate("clifornia");
+
+
+//solution 2 
+function translate(str) {
+  //finding all the consonants in the beginning of the str. 
+  //by using && below I'm achiving a concept called Intersection of Regular Expressions
+  //where your pattern finds the union of two or more RegEx rules. 
+  //In Java you can achive this with RegEx special character '&&' 
+  //but in Javascript you can use the following instead. 
+  //Credit goes to http://stackoverflow.com/q/6595477
+  var consChars = str.match(/^[a-z]/) && str.match(/[^aeiou]*/).join('');
+
+  //if no consonants found (i.e. str starts with vowels)
+  //use replace to remove consonants and construct newStr else add 'way' in the end
+  if (consChars !== ''){
+    newStr = str.replace(consChars, '') + consChars + "ay";
+  } else {
+    newStr = str + "way";
+  }
+  return newStr;
+}
