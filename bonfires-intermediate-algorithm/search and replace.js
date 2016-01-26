a581392d7d3834f4ecaeb9dbdf6807ce17907018
Rahul1992 <rahulreddy160@gmail.com>
2016-01-26 21:02:46 +0100

    search and replace sol 2

--- bonfires-intermediate-algorithm/search and replace.js
+++ bonfires-intermediate-algorithm/search and replace.js
@@ -10,3 +10,21 @@ function myReplace(str, before, after) {
 }
 
 myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped");
+
+
+// Solution 2:
+
+function myReplace(str, before, after) {
+//Create a regular expression object
+  var re = new RegExp(before,"gi");
+//Check whether the first letter is uppercase or not
+  if(/[A-Z]/.test(before[0])){
+  //Change the word to be capitalized
+    after = after.charAt(0).toUpperCase()+after.slice(1);
+     }
+     //Replace the original word with new one
+  var  newStr =  str.replace(re,after);
+
+ return newStr;
+
+}
