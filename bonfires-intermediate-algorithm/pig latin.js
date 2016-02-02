3eac7fa54d0609260376974181a65d74fb45cc32
Rahul1992 <rahulreddy160@gmail.com>
2016-02-02 20:19:18 +0100

    pig latin sol 4 added

--- bonfires-intermediate-algorithm/pig latin.js
+++ bonfires-intermediate-algorithm/pig latin.js
@@ -71,3 +71,17 @@ function translate(str) {
 }
 
 translate("consonant");
+
+
+
+// solution 4:
+function translate(str) {
+  function check(obj) {
+    return ['a','i','u','e','o'].indexOf(str.charAt(obj)) == -1 ?
+                                              check(obj+1) : obj;}
+
+  return str.substr(check(0))
+            .concat((check(0) === 0 ? 
+                        'w' : str.substr(0, check(0))) 
+                        + 'ay');
+}
