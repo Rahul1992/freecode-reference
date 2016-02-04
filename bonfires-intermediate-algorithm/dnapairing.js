09e1c86066a4dd64cdfa493892af59b420073579
Rahul1992 <rahulreddy160@gmail.com>
2016-02-05 00:52:02 +0100

    dna paring sol 2 added

--- bonfires-intermediate-algorithm/dnapairing.js
+++ bonfires-intermediate-algorithm/dnapairing.js
@@ -28,3 +28,18 @@ pair("GCG");
 // The program is very simple, the best solution that I have come up with is to use a switch to catch all the possible four elements. Using if statements would take too much code. You could also use Regular Expressions.
 // Since we have to the original and the pair, I decided to take all four cases instead of the base two.
 // Create an empty array and use the search function to push the right values to the array and return them.
+
+
+// solution 2:
+
+function pair(str) {
+  //define a map object with all pair possibilities 
+  var map = {T:'A', A:'T', G:'C', C:'G'};
+  //split str into a a char Array
+  strArr = str.split('');
+  //replace each Array item with a 2d Array using map
+  for (var i=0;i<strArr.length;i++){
+    strArr[i]=[strArr[i], map[strArr[i]]];
+  }
+ return strArr;
+}
